0d9204afdd61bf65669e2fffe556ff0036db4595
antoinepdev <antoinepdeveloper@gmail.com>
2026-06-23 08:58:09 -0400

    refactor(telegram-movies): adapt getTelegramMovies to database refactor

--- controllers/movies/getTelegramMovies.ts
+++ controllers/movies/getTelegramMovies.ts
@@ -2,13 +2,8 @@ import type { Response, Request } from "express";
 import { pool } from "../../database.ts";
 
 export async function getTelegramMovies(_: Request, res: Response) {
-  const result1 = await pool.query(
-    " SELECT * FROM raw_movies_es where (is_saved = false);",
-  );
-  const result2 = await pool.query(
-    " SELECT * FROM raw_movies_mx where (is_saved = false);",
-  );
-  const movies = [...result1.rows, ...result2.rows];
+  const result = await pool.query( " SELECT * FROM telegram_movies where (is_saved = false);",);
+  const movies = result.rows
 
   return res.status(200).json(movies);
 }
