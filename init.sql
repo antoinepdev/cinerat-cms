b11b6c49e36dec129efb55c8ea2d71bbd872fd82
antoinepdev <antoinepdeveloper@gmail.com>
2026-06-30 10:08:55 -0300

    refactor: adapt init.sql to database scheme structure

--- init.sql
+++ init.sql
@@ -1,46 +1,26 @@
-CREATE TABLE raw_movies_mx (
-  id serial primary key,
-  file_id int not null unique,
-  text text not null unique,
-  language text not null CHECK ( language = 'español latino 🇲🇽'),
-  is_saved boolean not null
-);
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY,
+    title_en TEXT NOT NULL,
+    title_cas TEXT,
+    title_lat TEXT,
+    year SMALLINT NOT NULL CHECK ( year > 1900 AND year <= 2026),
+    telegram_file_id_cas INT UNIQUE,
+    telegram_file_id_lat INT UNIQUE,
+    language_cas BOOLEAN DEFAULT FALSE,
+    language_lat BOOLEAN DEFAULT FALSE,
+    catalog_name TEXT NOT NULL,
+    catalog_version SMALLINT NOT NULL,
+    quality TEXT,
+    poster TEXT NOT NULL UNIQUE CHECK ( poster ~ '^https:\/\/.+' ),
+    telegram_poster_id INT NOT NULL,
+    telegram_container_group_id BIGINT NOT NULL,
+    description TEXT NOT NULL
+)
 
-CREATE TABLE raw_movies_es (
-  id serial primary key,
-  file_id int not null unique,
-  text text not null unique,
-  language text not null CHECK ( language = 'español castellano 🇪🇸'),
-  is_saved boolean not null
-);
-
-CREATE TABLE movies_mx (
-  title TEXT NOT NULL UNIQUE,
-  title_es TEXT[] NOT NULL UNIQUE,
-  year SMALLINT NOT NULL CHECK ( year > 1950 AND year <= 2026),
-  poster TEXT NOT NULL CHECK ( poster ~ '^https:\/\/.+' ),
-  language TEXT NOT NULL CHECK ( language = 'español latino 🇲🇽'),
-  quality TEXT NOT NULL CHECK ( quality = 'HD (1040p)' OR quality = 'Copia de cine' OR quality = '720p' OR quality = 'Indefinida'),
-  id SERIAL PRIMARY KEY,
-  description TEXT NOT NULL,
-  telegram_file_id INTEGER NOT NULL UNIQUE,
-  telegram_poster_id INTEGER NOT NULL,
-  telegram_container_group_id BIGINT NOT NULL,
-  catalog TEXT NOT NULL
-);
-
-
-CREATE TABLE movies_es (
-  title TEXT NOT NULL UNIQUE,
-  title_es TEXT[] NOT NULL UNIQUE,
-  year SMALLINT NOT NULL CHECK ( year > 1950 AND year <= 2026),
-  poster TEXT NOT NULL CHECK ( poster ~ '^https:\/\/.+' ),
-  language TEXT NOT NULL CHECK ( language = 'español castellano 🇪🇸'),
-  quality TEXT NOT NULL CHECK ( quality = 'HD (1040p)' OR quality = 'Copia de cine' OR quality = '720p' OR quality = 'Indefinida'),
-  id SERIAL PRIMARY KEY,
-  description TEXT NOT NULL,
-  telegram_file_id INTEGER NOT NULL UNIQUE,
-  telegram_poster_id INTEGER NOT NULL,
-  telegram_container_group_id BIGINT NOT NULL,
-  catalog TEXT NOT NULL
-);
+CREATE TABLE telegram_movies (
+    id SERIAL PRIMARY KEY,
+    file_id INT NOT NULL UNIQUE,
+    message_text NOT NULL,
+    language TEXT NOT NULL CHECK (language IN ('latino', 'castellano')),
+    is_saved BOOLEAN DEFAULT FALSE
+)
