687a8ed12b62c8b27165e110fa4a22f3153e8cbc
antoinepdev <antoinepdeveloper@gmail.com>
2026-08-26 18:49:21 -0300

    feat(routes): wire PATCH /movies endpoint with validation

--- backend/src/routes/movies.ts
+++ backend/src/routes/movies.ts
@@ -3,10 +3,12 @@ import { movieController } from '../controllers/movie.ts'
 import { errorHandler } from '../middlewares/errorHandler.ts'
 import { validateGetMoviesQueryParams } from '../middlewares/validateGetMoviesQueryParams.ts'
 import { validateSaveMovieBody } from '../middlewares/validateSaveMovieBody.ts'
+import { validateUpdateMovieBody } from '../middlewares/validateUpdateMovieBody.ts'
 
 const router = Router()
 router.get('/telegram', movieController.getTelegramMovieHandler)
 router.get('/', validateGetMoviesQueryParams, movieController.getMoviesHandler)
 router.post('/', validateSaveMovieBody, movieController.saveMovieHandler, errorHandler)
+router.patch('/', validateUpdateMovieBody, movieController.updateMovieHandler, errorHandler)
 
 export { router as moviesRouter }
