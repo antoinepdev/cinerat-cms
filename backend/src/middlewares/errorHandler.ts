813aa425b1ce39640e6621bd3bdfa60f2ec28327
antoinepdev <antoinepdeveloper@gmail.com>
2026-09-04 18:10:14 -0300

    fix(error-handler): resolve type errors by narrowing error with instanceof Error

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -2,16 +2,20 @@ import type { NextFunction, Request, Response } from 'express'
 import { NotFoundError } from '../utils/errors.ts'
 
 export async function errorHandler(error: unknown, _: Request, res: Response, __: NextFunction) {
-	if (error instanceof NotFoundError) return res.status(error.statusCode).json({ error: error.message })
+	if (error instanceof Error) {
+		if (error instanceof NotFoundError) return res.status(error.statusCode).json({ error: error.message })
 
-	if (error.code === 'ETELEGRAM') {
+		// telegram errors
 		if (error.message.includes('wrong type of the web page content')) {
 			return res.status(400).json({ error: 'Invalid poster url' })
 		} else if (error.message.includes('message to copy not found')) {
 			return res.status(400).json({ error: 'Invalid telegram_file_id' })
 		}
-	}
 
-	console.log(error.code, error.message)
-	return res.status(500).json({ error: 'Server internal error' })
+		// unexpected errors
+		else {
+			console.log(error.message)
+			return res.status(500).json({ error: 'Server internal error' })
+		}
+	}
 }
