379601b162055a8ee6094077b6f0559a728034de
antoinepdev <antoinepdeveloper@gmail.com>
2026-09-04 18:10:34 -0300

    fix(error-handler): respond 500 when thrown value is not an Error

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -8,14 +8,15 @@ export async function errorHandler(error: unknown, _: Request, res: Response, __
 		// telegram errors
 		if (error.message.includes('wrong type of the web page content')) {
 			return res.status(400).json({ error: 'Invalid poster url' })
-		} else if (error.message.includes('message to copy not found')) {
+		}
+		if (error.message.includes('message to copy not found')) {
 			return res.status(400).json({ error: 'Invalid telegram_file_id' })
 		}
 
-		// unexpected errors
-		else {
-			console.log(error.message)
-			return res.status(500).json({ error: 'Server internal error' })
-		}
+		console.log(error.message)
+		return res.status(500).json({ error: 'Server internal error' })
 	}
+
+	console.log(error)
+	return res.status(500).json({ error: 'Server internal error' })
 }
