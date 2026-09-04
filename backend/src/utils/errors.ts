0d5e717c8c2310f3cb8087125111ea4f7240c837
antoinepdev <antoinepdeveloper@gmail.com>
2026-09-04 17:17:13 -0300

    feat(errors): add NotFoundError class

--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -6,3 +6,9 @@ export class AppError extends Error {
 		this.statusCode = statusCode
 	}
 }
+
+export class NotFoundError extends AppError {
+	constructor(message: string = 'Movie not found') {
+		super(message, 404)
+	}
+}
