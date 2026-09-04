ac4d300b3735fd5ec2493812a22f115657b6db1c
antoinepdev <antoinepdeveloper@gmail.com>
2026-09-03 21:12:35 -0300

    fix: truncate poster caption when exceeding max char limit

--- backend/src/helpers/getPosterCaption.ts
+++ backend/src/helpers/getPosterCaption.ts
@@ -1,9 +1,16 @@
 import type { IMovieInput } from '../schemas/movie.ts'
 
+const TELEGRAM_CAPTION_CHARACTER_LIMIT = 1024
+const ELLIPSIS = '...'
+
 export async function getPosterCaption(movie: IMovieInput): Promise<string> {
 	const { title_en, title_cas, title_lat, year, description } = movie
 	const availableTitles = [title_en, title_cas, title_lat].filter(Boolean)
 
-	const posterCaption = `${availableTitles.join(' | ')}\n${year}\n\n${description}`
-	return posterCaption
+	const fullCaption = `${availableTitles.join(' | ')}\n${year}\n\n${description}`
+	if (fullCaption.length > TELEGRAM_CAPTION_CHARACTER_LIMIT) {
+		const posterCaption = `${fullCaption.slice(0, TELEGRAM_CAPTION_CHARACTER_LIMIT - ELLIPSIS.length)}${ELLIPSIS}`
+		return posterCaption
+	}
+	return fullCaption
 }
