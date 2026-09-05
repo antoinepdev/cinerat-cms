525b307e230ce0310e1997a1f360ce28bda09395
antoinepdev <antoinepdeveloper@gmail.com>
2026-09-05 11:10:00 -0300

    refactor(controllers): remove try/catch and rely on Express 5 async error forwarding

--- backend/src/controllers/movie.ts
+++ backend/src/controllers/movie.ts
@@ -1,41 +1,25 @@
-import type { NextFunction, Request, Response } from 'express'
+import type { Request, Response } from 'express'
 import { movieService } from '../services/movies.ts'
 
-async function getTelegramMovieHandler(_: Request, res: Response, next: NextFunction) {
-	try {
-		const telegramMovies = await movieService.getTelegramMovies()
-		return res.status(200).json(telegramMovies)
-	} catch (error) {
-		next(error)
-	}
+async function getTelegramMovieHandler(_: Request, res: Response) {
+	const telegramMovies = await movieService.getTelegramMovies()
+	return res.status(200).json(telegramMovies)
 }
 
-async function getMoviesHandler(req: Request, res: Response, next: NextFunction) {
-	try {
-		const filters = req.filteredQuery
-		const movies = await movieService.getMovies(filters)
-		return res.status(200).json(movies)
-	} catch (error) {
-		next(error)
-	}
+async function getMoviesHandler(req: Request, res: Response) {
+	const filters = req.filteredQuery
+	const movies = await movieService.getMovies(filters)
+	return res.status(200).json(movies)
 }
 
-async function saveMovieHandler(req: Request, res: Response, next: NextFunction) {
-	try {
-		const movie = await movieService.saveMovie(req.body)
-		return res.status(201).json(movie)
-	} catch (error) {
-		next(error)
-	}
+async function saveMovieHandler(req: Request, res: Response) {
+	const movie = await movieService.saveMovie(req.body)
+	return res.status(201).json(movie)
 }
 
-async function updateMovieHandler(req: Request, res: Response, next: NextFunction) {
-	try {
-		const updatedMovie = await movieService.updateMovie(req.body)
-		return res.status(201).json(updatedMovie)
-	} catch (error) {
-		next(error)
-	}
+async function updateMovieHandler(req: Request, res: Response) {
+	const updatedMovie = await movieService.updateMovie(req.body)
+	return res.status(201).json(updatedMovie)
 }
 
 const movieController = {
