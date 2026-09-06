246e6aee30e80329b2a617d903813e8e7d0bca6c
antoinepdev <antoinepdeveloper@gmail.com>
2026-09-06 10:40:48 -0300

    feat(error-handler): handle ValidationError

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,9 +1,10 @@
 import type { NextFunction, Request, Response } from 'express'
-import { NotFoundError } from '../utils/errors.ts'
+import { NotFoundError, ValidationError } from '../utils/errors.ts'
 
 export function errorHandler(error: unknown, _: Request, res: Response, __: NextFunction) {
 	if (error instanceof Error) {
 		if (error instanceof NotFoundError) return res.status(error.statusCode).json({ error: error.message })
+		if (error instanceof ValidationError) return res.status(error.statusCode).json({ error: error.message })
 
 		// telegram errors
 		if (error.message.includes('wrong type of the web page content')) {
