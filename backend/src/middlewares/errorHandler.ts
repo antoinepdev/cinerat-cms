10f86f0dadd80050573f8863e2ef47a1a0958207
antoinepdev <antoinepdeveloper@gmail.com>
2026-09-09 10:47:50 -0400

    refactor(error-handler): use RFC 9457 error format

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,23 +1,27 @@
 import type { NextFunction, Request, Response } from 'express'
+import { buildProblemDetailsError } from '../helpers/buildProblemDetailsError.ts'
 import { NotFoundError, ValidationError } from '../utils/errors.ts'
+
+function sendErrorResponse({ res, message, status }: { res: Response; message: string; status: number }) {
+	return res.status(status).json(buildProblemDetailsError({ message, status }))
+}
 
 export function errorHandler(error: unknown, _: Request, res: Response, __: NextFunction) {
 	if (error instanceof Error) {
-		if (error instanceof NotFoundError) return res.status(error.status).json({ error: error.message })
-		if (error instanceof ValidationError) return res.status(error.status).json({ error: error.message })
+		if (error instanceof NotFoundError) return sendErrorResponse({ res, message: error.message, status: error.status })
+		if (error instanceof ValidationError) return sendErrorResponse({ res, message: error.message, status: error.status })
 
 		// telegram errors
-		if (error.message.includes('wrong type of the web page content')) {
-			return res.status(400).json({ error: 'Invalid poster url' })
-		}
-		if (error.message.includes('message to copy not found')) {
-			return res.status(400).json({ error: 'Invalid telegram_file_id' })
-		}
+		if (error.message.includes('wrong type of the web page content'))
+			return sendErrorResponse({ res, message: 'Invalid poster url', status: 422 })
+
+		if (error.message.includes('message to copy not found'))
+			return sendErrorResponse({ res, message: 'Invalid telegram_file_id', status: 422 })
 
 		console.log(error.message)
-		return res.status(500).json({ error: 'Server internal error' })
+		return sendErrorResponse({ res, message: 'Server internal error', status: 500 })
 	}
 
 	console.log(error)
-	return res.status(500).json({ error: 'Server internal error' })
+	return sendErrorResponse({ res, message: 'Server internal error', status: 500 })
 }
