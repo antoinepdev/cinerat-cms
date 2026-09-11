e6e3e46a0fbb41788f0fc16dd4c5a92f713f0773
antoinepdev <antoinepdeveloper@gmail.com>
2026-09-11 12:27:04 -0400

    refactor(errors): simplify unexpected error handling

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,6 +1,12 @@
 import type { NextFunction, Request, Response } from 'express'
 import { buildProblemDetailsError } from '../helpers/buildProblemDetailsError.ts'
-import { type IFieldError, InvalidPosterUrlError, InvalidTelegramFileIdError, NotFoundError, ValidationError } from '../utils/errors.ts'
+import {
+	type IFieldError,
+	InvalidPosterUrlError,
+	InvalidTelegramFileIdError,
+	NotFoundError,
+	ValidationError,
+} from '../utils/errors.ts'
 
 interface IProps {
 	res: Response
@@ -14,17 +20,12 @@ function sendErrorResponse({ res, message, status, errors }: IProps) {
 }
 
 export function errorHandler(error: unknown, _: Request, res: Response, __: NextFunction) {
-	if (error instanceof Error) {
-		if (error instanceof NotFoundError) return sendErrorResponse({ res, message: error.message, status: error.status })
-		if (error instanceof ValidationError)
-			return sendErrorResponse({ res, message: error.message, status: error.status, errors: error.errors })
+	if (error instanceof NotFoundError) return sendErrorResponse({ res, message: error.message, status: error.status })
+	if (error instanceof ValidationError)
+		return sendErrorResponse({ res, message: error.message, status: error.status, errors: error.errors })
 
-		if (error instanceof InvalidPosterUrlError) return sendErrorResponse({ res, message: error.message, status: error.status })
-		if (error instanceof InvalidTelegramFileIdError) return sendErrorResponse({ res, message: error.message, status: error.status })
-
-		console.log(error.message)
-		return sendErrorResponse({ res, message: 'Server internal error', status: 500 })
-	}
+	if (error instanceof InvalidPosterUrlError) return sendErrorResponse({ res, message: error.message, status: error.status })
+	if (error instanceof InvalidTelegramFileIdError) return sendErrorResponse({ res, message: error.message, status: error.status })
 
 	console.log(error)
 	return sendErrorResponse({ res, message: 'Server internal error', status: 500 })
