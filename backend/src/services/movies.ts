827dc591559e78fa78f91c3c8678c4693fb4d5c3
antoinepdev <antoinepdeveloper@gmail.com>
2026-09-13 10:54:00 -0400

    style(movies): format telegram file ids line

--- backend/src/services/movies.ts
+++ backend/src/services/movies.ts
@@ -15,9 +15,7 @@ async function getTelegramMovies() {
 }
 
 async function saveMovie(movie: IMovieInput): Promise<IMovie> {
-	const old_telegram_file_ids = [movie.telegram_file_id_cas, movie.telegram_file_id_lat].filter(
-		(id): id is number => Boolean(id)
-	)
+	const old_telegram_file_ids = [movie.telegram_file_id_cas, movie.telegram_file_id_lat].filter((id): id is number => Boolean(id))
 
 	const movieToSave: IMovieToSave = await telegramService.saveMovie(movie)
 	const savedMovie = await movieRepository.saveMovie({ ...movieToSave, quality: undefined })
