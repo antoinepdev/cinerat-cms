fef7292605848094e3d854a2bfb0a19d6059b575
antoinepdev <antoinepdeveloper@gmail.com>
2026-09-14 10:37:53 -0400

    feat(error-handler): handle telegram api errors

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,7 +1,9 @@
 import type { NextFunction, Request, Response } from 'express'
 import { DatabaseError } from 'pg'
 import { buildProblemDetailsError } from '../helpers/buildProblemDetailsError.ts'
+import { isTelegramApiError } from '../helpers/isTelegramApiError.ts'
 import { mapDatabaseError } from '../helpers/mapDatabaseError.ts'
+import { mapTelegramError } from '../helpers/mapTelegramError.ts'
 import { AppError, type IFieldError } from '../utils/errors.ts'
 
 interface IProps {
@@ -19,6 +21,10 @@ export function errorHandler(error: unknown, _: Request, res: Response, __: Next
 	if (error instanceof DatabaseError) {
 		const dbError = mapDatabaseError(error)
 		if (dbError) return sendErrorResponse({ res, message: dbError.message, status: dbError.status, errors: dbError.errors })
+	}
+	if (isTelegramApiError(error)) {
+		const tgError = mapTelegramError(error)
+		if (tgError) return sendErrorResponse({ res, message: tgError.message, status: tgError.status })
 	}
 	if (error instanceof AppError)
 		return sendErrorResponse({ res, message: error.message, status: error.status, errors: error.errors })
