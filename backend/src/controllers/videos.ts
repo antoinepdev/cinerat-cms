50e09b5edfcbb869eaf61cb3e45a05408178da9a
antoinepdev <antoinepdeveloper@gmail.com>
2026-09-24 11:15:54 -0400

    feat(backend/controllers): add deleteIncomingVideoHandler

--- backend/src/controllers/videos.ts
+++ backend/src/controllers/videos.ts
@@ -6,8 +6,15 @@ async function getPendingVideosHandler(_: Request, res: Response) {
 	return res.status(200).json(pendingVideos)
 }
 
+async function deleteIncomingVideoHandler(req: Request, res: Response) {
+	const id = req.filteredParams!.id
+	await videoService.deleteIncomingVideo(id)
+	return res.status(204).send()
+}
+
 const videoController = {
 	getPendingVideosHandler,
+	deleteIncomingVideoHandler,
 }
 
 export { videoController }
